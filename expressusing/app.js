2945a265d179bb401118d0a14ca7474a70e1b1f0
tanya3007 <msstanya.30899@gmail.com>
2021-07-07 18:11:02 +0530

    cors error resolved

--- expressusing/app.js
+++ expressusing/app.js
@@ -1,23 +1,33 @@
-const path = require('path');
+// const path = require('path');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const logger = require("morgan");
 
-app.use(express.urlencoded({extented: false}));
+app.use(cors())
 
-arr = ["item 1" , "item 2", "item3"];
+// app.use(express.urlencoded({extented: false}));
+
+// const staticPath = (path.join(__dirname))
+
+var arr = ["item 1" , "item 2", "item3"];
+arr.push("item 4", "item 5")
 app.get("/",(req,res) => {
     res.send(arr);
 });
 
-app.get("/about",(req,res) => {
-    res.send("hello world from about");
+// app.get("/about",(req,res) => {
+//     res.send("hello world from about");
+// });
+
+app.post('/new', (req,res) => {
+    const post = req.body;
+    console.log(post);
+    post.push(post);
+    res.send('Done');
 });
 
-app.post('/', (req,res) => {
-    console.log(req.body);
-    res.send('created user');
-});
-
-app.listen(3000, () => {
-    console.log("listening the port at 3000");
+app.listen(8000, () => {
+    console.log("listening the port at 8000");
 });
